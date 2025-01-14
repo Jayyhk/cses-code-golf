f14e6e5027be6435429bbbb4c7f0baa0bfd3e811
Jayyhk <jemallen10@gmail.com>
2025-01-14 17:36:15 -0500

    added twoknights + updates

--- Geometry/PointLocationTestGolf.py
+++ Geometry/PointLocationTestGolf.py
@@ -2,4 +2,4 @@ I=input
 for _ in' '*int(I()):
     a,b,c,d,e,f=map(int,I().split())
     o=(d-b)*(e-c)-(f-d)*(c-a)
-    print(["TOUCH",["RIGHT","LEFT"][o<0]][o!=0])
+    print("TRLOIEUGFCHTHT"[(o!=0)+(o<0)::3])
---
+++ IntroductoryProblems/TwoKnights.py
@@ -0,0 +1,6 @@
+n = int(input())
+
+for k in range(1, n + 1):
+    total = (k ** 2) * (k ** 2 - 1) // 2
+    attack = 2 * (k - 1) * (k - 2) + 2 * (k - 2) * (k - 1)
+    print(total - attack)
