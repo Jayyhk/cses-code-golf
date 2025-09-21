27466c6efff326a0265047cb78b3dd5e622d0f9b
Jake Mallen <jemallen10@gmail.com>
2025-09-20 22:11:33 -0400

    add LICENSE

--- char_counter.py
+++
@@ -1,7 +0,0 @@
-with open("code.txt", "r") as file:  # copy your code to code.txt
-    content = file.read()
-
-# Count non-whitespace characters
-non_whitespace_count = sum(1 for char in content if not char.isspace())
-
-print(f"Number of non-whitespace characters: {non_whitespace_count}")
